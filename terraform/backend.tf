058016a785f226b94ed127a52da6e57df0fa356a
Nate <stevic.nenad@icloud.com>
2025-05-24 14:55:54 +0200

    Add remote backend configuration

---
+++ terraform/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "nenad-stevic-nextcloud-tfstate"
+    key            = "nextcloud-project/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "nenad-stevic-nextcloud-tfstate-lock"
+    encrypt        = true
+    profile        = "nextcloud-project"
+  }
+}
